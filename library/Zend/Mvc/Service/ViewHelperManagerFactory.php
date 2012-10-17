zendframework/zendframework

commit a4cb96439ada36544cecd1fc17c00a5648b60d39

Added isset check for the 'view_manager' key to prevent undefined index notice.

library/Zend/Mvc/Service/ViewHelperManagerFactory.php
@@ -84,9 +84,8 @@ class ViewHelperManagerFactory extends AbstractPluginManagerFactory
 
         $plugins->setFactory('basepath', function($sm) use($serviceLocator) {
             $config = $serviceLocator->get('Config');
-            $config = $config['view_manager'];
             $basePathHelper = new ViewHelper\BasePath;
-            if (isset($config['base_path'])) {
+            if (isset($config['view_manager']) && isset($config['view_manager']['base_path'])) {
                 $basePath = $config['base_path'];
             } else {
                 $basePath = $serviceLocator->get('Request')->getBasePath();

samsonasik/zend-stdlib

commit 473787fe9219772d1184325a9d7a7a0ba5c63d9f

[+] bugfix: #2781 Commit a4cb96439ada36544cecd1fc17c00a5648b60d39 broke view_path parameter.

library/Zend/Mvc/Service/ViewHelperManagerFactory.php
@@ -86,7 +86,7 @@ class ViewHelperManagerFactory extends AbstractPluginManagerFactory
             $config = $serviceLocator->get('Config');
             $basePathHelper = new ViewHelper\BasePath;
             if (isset($config['view_manager']) && isset($config['view_manager']['base_path'])) {
-                $basePath = $config['base_path'];
+                $basePath = $config['view_manager']['base_path'];
             } else {
                 $basePath = $serviceLocator->get('Request')->getBasePath();
             }