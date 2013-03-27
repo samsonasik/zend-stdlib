zendframework/zendframework

Adding code to allow for input validation to continue if value is empty instead of just being marked as valid if empty.  Required to test for validity of an empty value based on context.

library/Zend/InputFilter/BaseInputFilter.php
@@ -176,9 +176,13 @@ class BaseInputFilter implements InputFilterInterface, UnknownInputsCapableInter
                         continue;
                     }
                     // - test if input allows empty
+                    // - continueIfEmpty allows for context validation
+                    // for empty values
                     if ($input->allowEmpty()) {
-                        $this->validInputs[$name] = $input;
-                        continue;
+                         if (!$input->continueIfEmpty()) {
+                            $this->validInputs[$name] = $input;
+                            continue;
+                        }
                     }
                 }
                 // make sure we have a value (empty) for validation

samsonasik/zend-stdlib

library/Zend/InputFilter/BaseInputFilter.php
@@ -187,8 +187,10 @@ class BaseInputFilter implements InputFilterInterface, UnknownInputsCapableInter
                 && $input->isRequired()
                 && $input->allowEmpty()
             ) {
-                $this->validInputs[$name] = $input;
-                continue;
+                if (!$input->allowEmpty()) {
+                    $this->validInputs[$name] = $input;
+                    continue;
+                }
             }
 
             // key exists, is null, input is not required; valid